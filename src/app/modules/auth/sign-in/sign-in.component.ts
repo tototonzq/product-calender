66b6707c2addd72aa8ac9ea719967dbe738f6bbc
_tototonzq <117251703+tototonzq@users.noreply.github.com>
2023-09-11 23:10:55 +0700

    [ ADD ]

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
+  /* -------------------------------------------------------------------------- */
+  /*                                 Constructor                                */
+  /* -------------------------------------------------------------------------- */
   constructor() {}
 
+  /* -------------------------------------------------------------------------- */
+  /*                                 Life Circle                                */
+  /* -------------------------------------------------------------------------- */
   ngOnInit() {}
+
+  /* -------------------------------------------------------------------------- */
+  /*                                  Variables                                 */
+  /* -------------------------------------------------------------------------- */
+  activeIndex: number = 0;
+  images: any[] | undefined = [
+    {
+      itemImageSrc:
+        'http://49.0.80.15/DEMO-WAREHOUSE/assets/img/qa-engineers3.svg',
+    },
+    {
+      itemImageSrc:
+        'http://49.0.80.15/DEMO-WAREHOUSE/assets/img/qa-engineers3.svg',
+    },
+  ];
 }
--- src/app/modules/auth/sign-in/sign-in.module.ts
+++ src/app/modules/auth/sign-in/sign-in.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SignInComponent } from './sign-in.component';
 import { RouterModule } from '@angular/router';
 import { signInRoutes } from './sign-in.routing';
+import { ShareModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(signInRoutes)],
+  imports: [CommonModule, ShareModule, RouterModule.forChild(signInRoutes)],
   declarations: [SignInComponent],
 })
 export class SignInModule {}
